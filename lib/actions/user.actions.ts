d8dcff8de588b64eeaabc801268604208a1fbf33
Nhật Nguyễn Đình <nhatnguyendinh@Nhats-MacBook-Air.local>
2023-12-11 10:20:36 +0700

    update profile page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,8 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
+import Community from "../models/community.model";
 
 interface Params {
   userId: string;
@@ -11,6 +13,19 @@ interface Params {
   bio: string;
   image: string;
   path: string;
+}
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+
+    return await User.findOne({ id: userId }).populate({
+      path: "communities",
+      model: Community,
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
 }
 
 export async function updateUser({
@@ -44,12 +59,35 @@ export async function updateUser({
   }
 }
 
-export async function fetchUser(userId: string) {
+export async function fetchUserPosts(userId: string) {
   try {
     connectToDB();
 
-    return await User.findOne({ is: userId });
-  } catch (error: any) {
-    throw new Error(`Failed to fetch user: ${error.message}`);
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ is: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: [
+        {
+          path: "community",
+          model: Community,
+          select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+        },
+        {
+          path: "children",
+          model: Thread,
+          populate: {
+            path: "author",
+            model: User,
+            select: "name image id", // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+
+    return threads;
+  } catch (error) {
+    console.error("Error fetching user threads:", error);
+    throw error;
   }
 }
